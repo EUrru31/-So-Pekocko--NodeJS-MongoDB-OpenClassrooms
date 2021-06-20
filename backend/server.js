b02cfb39352c4b84cd25eaab05bdeba5552a1e1f
Urru <etienne.urru@gmail.com>
2021-06-20 10:32:13 +0200

    installation nvm

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const http = require("http");
 
 const app = require("./app");
 
@@ -13,7 +13,7 @@ const normalizePort = (val) => {
     }
     return false;
 };
-const port = normalizePort(process.env.PORT || "4200");
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 const errorHandler = (error) => {
